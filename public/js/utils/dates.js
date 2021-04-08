06ee89c9114ba4fd486b2989fa03a8b654b5b84c
Caterina Turnbull <caterina.turnbull@gmail.com>
2021-04-09 03:20:20 +1000

    Fix current date returning wrong month

--- public/js/utils/dates.js
+++ public/js/utils/dates.js
@@ -2,7 +2,7 @@
 function nowToInput() {
   const date = new Date()
   const year = date.getFullYear().toString()
-  const month = date.getMonth().toString().padStart(2, '0')
+  const month = (date.getMonth() + 1).toString().padStart(2, '0')
   const day = date.getDate().toString().padStart(2, '0')
   const time = date.toString().slice(16, 21)
   return year + '-' + month + '-' + day + 'T' + time
